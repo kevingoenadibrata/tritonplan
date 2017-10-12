42b357940481033575046f553ed673ec57109f70
Kevin Goenadibrata <kevingoenadibrata@gmail.com>
2017-10-11 17:32:19 -0700

    added search box

--- src/index.js
+++ src/index.js
@@ -12,4 +12,4 @@ const About = () => {
   return <p><t />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut arcu urna. Suspendisse gravida felis non odio efficitur rhoncus. Etiam varius, erat a elementum sagittis, sem tellus fermentum mauris, in ultricies dolor nulla quis enim. Sed ac est sed felis suscipit consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sagittis, nisl vel porta rhoncus, nibh mi aliquam elit, at scelerisque justo felis at urna. In suscipit posuere nulla, eu tristique tellus bibendum id. <br /> <br /> <t />Curabitur pretium, enim eget posuere malesuada, lorem nunc scelerisque massa, in tincidunt ipsum ex sit amet turpis. Pellentesque id molestie odio, ut bibendum lorem. Maecenas dictum libero id nibh congue dapibus. Aliquam quis consectetur dui. Aliquam nec fringilla magna. Morbi in laoreet eros. Mauris in fringilla massa, sit amet lobortis odio. Integer et turpis velit. Vivamus eget sem sed sapien accumsan rhoncus.</p>
 }
 
-ReactDOM.render(<About />, document.querySelector('.window'));
+ReactDOM.render(<About />, document.querySelector('.window-content'));
